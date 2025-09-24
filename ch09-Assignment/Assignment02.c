2bd873ee30e2645b54ba59168aa476a02ee09f93
user <user@DESKTOP-7RRVOCK>
2025-09-24 11:38:10 +0900

    초기 커밋.

---
+++ ch09-Assignment/Assignment02.c
@@ -0,0 +1,11 @@
+/*
+학번: 202511859
+이름: 최정현
+프로그램명:Assignment02 (문자 배열을 매개변수로 전달받아 공백 문자를 모두 제거하는 remove_space 함수 작성)
+날짜: 25.09.24
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
